2d9e6ebf8ccc2a20a5920b23d327e283f5812fa0
tapanbasak <tapanonly@gmail.com>
2012-02-26 16:29:20 +0530

    Update testblob.php

--- testblob.php
+++ testblob.php
@@ -3,7 +3,7 @@
    mysql_select_db("drupal_city", $cp) or die('there is a problem in database connection.');
    $q = mysql_query("SELECT `data` FROM `field_config_instance` WHERE `id`=17");
    $d = mysql_fetch_assoc($q);
-   print '<pre>';
+   print '<pre>111';
    print_r(unserialize($d['data']));
    
 ?>
