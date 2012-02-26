7b7c7c0836393bb354c8c0f270ef2c9ee761554d
Tapan Basak <tapanonly@gmail.com>
2012-02-26 15:56:45 +0530

    Add a README file to help explain the project

---
+++ testblob.php
@@ -0,0 +1,9 @@
+<?php
+   $cp = mysql_connect("localhost", "root", "rootwdp");
+   mysql_select_db("drupal_city", $cp) or die('there is a problem in database connection.');
+   $q = mysql_query("SELECT `data` FROM `field_config_instance` WHERE `id`=17");
+   $d = mysql_fetch_assoc($q);
+   print '<pre>';
+   print_r(unserialize($d['data']));
+   
+?>
